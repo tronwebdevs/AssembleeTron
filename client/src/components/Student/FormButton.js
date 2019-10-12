804b7f7536d95bc53c3aab41687aa2b22867ceb9
Davide Testolin <davide150602@gmail.com>
2019-10-12 10:29:56 +0200

    Fixing and improvements

---
+++ client/src/components/Student/FormButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from 'tabler-react';
+import { Spinner } from 'reactstrap';
+
+const FormButton = ({
+    text,
+    isSubmitting
+}) => (
+	<Button type="submit" color="primary" block={true} disabled={isSubmitting}>
+		{isSubmitting ? <Spinner color="light" size="sm" /> : text}
+	</Button>
+);
+
+FormButton.propTypes = {
+    text: PropTypes.string.isRequired,
+    isSubmitting: PropTypes.bool.isRequired
+};
+
+export default FormButton;
