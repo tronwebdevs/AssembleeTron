78504ef112f15c1e6cfe26bf72ef54092922cbcf
Davide Testolin <davide150602@gmail.com>
2019-12-09 15:11:52 +0100

    Remove CORS

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/dissembly', {
@@ -19,9 +18,6 @@ app.disable('x-powered-by');
 app.use(require('helmet')());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors({
-    origin: 'https://assemblee.tronweb.it'
-}));
 
 app.use('/api/students', require('./routes/students'));
 app.use('/api/assembly', require('./routes/assembly'));
